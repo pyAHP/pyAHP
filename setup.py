e5510b1ac95fb9722338a4aefa28ae1467dcafb0
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2018-02-14 23:52:06 +0000

    Add basic project structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyAHP",
+    version="0.1",
+    packages=find_packages()
+
+    author="Abhinav Mishra",
+    description="This is an Example Package",
+    license="MIT",
+    keywords="ahp analytic hierarchy process",
+    project_urls={
+        "Source Code": "https://github.com/pyAHP/pyAHP"
+    }
+
+)
