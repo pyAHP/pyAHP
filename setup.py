601ef1fbe5a5527ef94043e07def68139ace5bc8
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2018-03-01 01:24:47 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pyAHP",
-    version="0.1",
+    name='pyAHP',
+    version='0.1',
     packages=find_packages(),
 
-    author="Abhinav Mishra",
-    description="This is an Example Package",
-    license="MIT",
-    keywords="ahp analytic hierarchy process",
-    project_urls={
-        "Source Code": "https://github.com/pyAHP/pyAHP"
-    }
-
+    author='Abhinav Mishra',
+    description='This is an Example Package',
+    license='MIT',
+    url='https://github.com/pyAHP/pyAHP',
+    download_url='https://github.com/pyAHP/pyAHP/archive/0.1.tar.gz',
+    keywords=['ahp', 'analytic', 'hierarchy', 'process'],
 )
