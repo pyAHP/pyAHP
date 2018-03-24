6dd0d0d57b66929c0d0624838ae049ef38c83a5b
William Song <30965609+Freakwill@users.noreply.github.com>
2018-03-24 20:01:51 +0800

    Update geometric.py

--- pyahp/methods/geometric.py
+++ pyahp/methods/geometric.py
@@ -15,5 +15,5 @@ class GeometricMethod(Method):
 
     def estimate(self, preference_matrix):
         super()._check_matrix(preference_matrix)
-
-        return np.array([np.prod(row)**(1/len(row)) for row in preference_matrix])
+        coln = preference_matrix.shape[1]
+        return np.prod(preference_matrix, axis=1) ** (1/coln)
