3144896dcb023f24897c33ccb619368e92a7f972
William Song <30965609+Freakwill@users.noreply.github.com>
2018-03-30 19:40:08 +0800

    Update utils.py
    Simplify the codes.

--- pyahp/utils.py
+++ pyahp/utils.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """pyahp.utils
-
 This module contains the common functions and methods used by other modules in the class.
 """
 
@@ -9,17 +8,11 @@ import numpy as np
 
 def normalize_priorities(criteria_pr, global_pr):
     """Normalize the priorities received from the lower layer.
-
     This function performs a Global Prioritization at the current node.
-
-
     Args:
         criteria_pr (list(list(float))): The priorities of all the alternatives/sub-criteria.
         global_pr (list(float)): The global priorities of all criteria.
-
     Returns:
-       Normalized priorities (1D-array): crit_pr * matrix of priorities of criteria
+       Normalized priorities
     """
-    
-    crit_attr_pr = np.array([criterion.get_priorities() for criterion in criteria])
-    return np.dot(crit_pr, crit_attr_pr)
+    return np.dot(global_pr, criteria_pr)
