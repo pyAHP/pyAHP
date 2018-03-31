93e10d335486e5cf97af7bf35e3c0d1236ccac45
William Song <30965609+Freakwill@users.noreply.github.com>
2018-03-31 17:40:49 +0800

    Add files via upload
    add a new method

--- pyahp/methods/geometric.py
+++ pyahp/methods/geometric.py
@@ -16,4 +16,5 @@ class GeometricMethod(Method):
     def estimate(self, preference_matrix):
         super()._check_matrix(preference_matrix)
         height = preference_matrix.shape[1]
-        return np.prod(preference_matrix, axis=1) ** (1/height)
+        vec = np.prod(preference_matrix, axis=1) ** 1/height
+        return vec / np.sum(vec)
