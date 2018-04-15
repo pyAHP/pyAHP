cda9e91ab36ce378496435281658ac10c8611246
Abhinav Mishra <mishrabhinav96@gmail.com>
2018-04-14 18:26:44 -0700

    Add more assertions to the tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -14,8 +14,12 @@ def test_empty_model():
 def test_non_dict_model():
     model = []
 
-    with pytest.raises(AHPTypeMismatchError):
+    with pytest.raises(AHPTypeMismatchError) as err:
         parse(model)
+
+    assert err.value.var == 'model'
+    assert err.value.expected == 'dict'
+    assert err.value.actual == 'list'
 
 
 def test_model_with_missing_method():
@@ -30,8 +34,10 @@ def test_model_with_missing_method():
         }
     }
 
-    with pytest.raises(AHPFieldEmptyError):
+    with pytest.raises(AHPFieldEmptyError) as err:
         parse(model)
+
+    assert err.value.var == 'method'
 
 
 def test_model_with_non_string_method():
@@ -47,8 +53,12 @@ def test_model_with_non_string_method():
         }
     }
 
-    with pytest.raises(AHPTypeMismatchError):
+    with pytest.raises(AHPTypeMismatchError) as err:
         parse(model)
+
+    assert err.value.var == 'method'
+    assert err.value.expected == 'str'
+    assert err.value.actual == 'list'
 
 
 def test_model_with_unsupported_method():
@@ -64,8 +74,10 @@ def test_model_with_unsupported_method():
         }
     }
 
-    with pytest.raises(AHPMethodUnsupportedError):
+    with pytest.raises(AHPMethodUnsupportedError) as err:
         parse(model)
+
+    assert err.value.actual == 'ahp'
 
 
 def test_model_with_non_string_criteria():
@@ -81,8 +93,12 @@ def test_model_with_non_string_criteria():
         }
     }
 
-    with pytest.raises(AHPTypeMismatchError):
+    with pytest.raises(AHPTypeMismatchError) as err:
         parse(model)
+
+    assert err.value.var == 'criteria'
+    assert err.value.expected == 'str'
+    assert err.value.actual == 'int'
 
 
 def test_model_with_missing_criteria_pm():
@@ -97,8 +113,34 @@ def test_model_with_missing_criteria_pm():
         }
     }
 
-    with pytest.raises(AHPMissingPreferenceMatrixError):
+    with pytest.raises(AHPMissingPreferenceMatrixError) as err:
         parse(model)
+
+    assert err.value.kind == 'criteria'
+    assert err.value.name == 'criteria'
+
+
+def test_model_with_non_square_criteria_pm():
+    model = {
+        'method': 'geometric',
+        'criteria': ['A', 'B'],
+        'subCriteria': {},
+        'alternatives': ['C', 'D', 'E'],
+        'preferenceMatrices': {
+            'criteria': [[1, 2, 0], [0.5, 1, 0]],
+            'alternatives:A': [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            'alternatives:B': [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+        }
+    }
+
+    with pytest.raises(AHPNonSquarePreferenceMatrixError) as err:
+        parse(model)
+
+    assert err.value.kind == 'criteria'
+    assert err.value.name == 'criteria'
+    assert err.value.side == 2
+    assert err.value.actual_width == 2
+    assert err.value.actual_height == 3
 
 
 def test_model_with_non_string_alternatives():
@@ -114,5 +156,51 @@ def test_model_with_non_string_alternatives():
         }
     }
 
-    with pytest.raises(AHPTypeMismatchError):
+    with pytest.raises(AHPTypeMismatchError) as err:
         parse(model)
+
+    assert err.value.var == 'alternatives'
+    assert err.value.expected == 'str'
+    assert err.value.actual == 'int'
+
+
+def test_model_with_missing_alternative_pm():
+    model = {
+        'method': 'geometric',
+        'criteria': ['A', 'B'],
+        'subCriteria': {},
+        'alternatives': ['C', 'D', 'E'],
+        'preferenceMatrices': {
+            'criteria': [[1, 2], [0.5, 1]],
+            'alternatives:A': [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+        }
+    }
+
+    with pytest.raises(AHPMissingPreferenceMatrixError) as err:
+        parse(model)
+
+    assert err.value.kind == 'alternatives'
+    assert err.value.name == 'B'
+
+
+def test_model_with_non_square_alternative_pm():
+    model = {
+        'method': 'geometric',
+        'criteria': ['A', 'B'],
+        'subCriteria': {},
+        'alternatives': ['C', 'D', 'E'],
+        'preferenceMatrices': {
+            'criteria': [[1, 2], [0.5, 1]],
+            'alternatives:A': [[1, 1], [1, 1], [1, 1]],
+            'alternatives:B': [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+        }
+    }
+
+    with pytest.raises(AHPNonSquarePreferenceMatrixError) as err:
+        parse(model)
+
+    assert err.value.kind == 'alternatives'
+    assert err.value.name == 'A'
+    assert err.value.side == 3
+    assert err.value.actual_width == 3
+    assert err.value.actual_height == 2
