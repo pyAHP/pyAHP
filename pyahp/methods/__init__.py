8f25fbbfa5d6ab998df4d49d8447a0864155301e
William Song <30965609+Freakwill@users.noreply.github.com>
2018-05-09 20:55:45 +0800

    Add files via upload
    add a variable `availableMethods`. One only need modify the variable for extension, instead of affecting other files.

--- pyahp/methods/__init__.py
+++ pyahp/methods/__init__.py
@@ -9,3 +9,5 @@ from .approximate import ApproximateMethod
 from .eigenvalue import EigenvalueMethod
 from .geometric import GeometricMethod
 from .power import PowerMethod
+
+availableMethods = ('approximate', 'eigenvalue', 'geometric', 'power')
